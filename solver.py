32f1814980a3cbb08efd39b709c0b739f830700f
Bassem-A <73930604+Bassem-A@users.noreply.github.com>
2023-03-15 07:28:29 +0300

    Better comments

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 # Shuffle auto solver
 
-x = "ABCDEFGHIJKLMNOPQRSTUVWXYZabc" #Pre Shuffle (Same Length as flag needed)
-y = "BDFHJLNPRTVXZbAEIMQUYcGOWCSKa" #Post Shuffle (Same Length as flag needed)
+x = "ABCDEFGHIJKLMNOPQRSTUVWXYZabc" #Pre Shuffle (Same Length as flag needed, just change length as needed, make sure every character is unique)
+y = "BDFHJLNPRTVXZbAEIMQUYcGOWCSKa" #Post Shuffle (We got this after the shuffle happened)
 solution = ""
 dex = []
 #ignore = []
@@ -13,7 +13,7 @@ for i in range(0,len(x)):
             dex.append(j)
             
             
-z = "lgcnyurVr34d0Df{__3_R}43na501" #Shuffled Flag
+z = "lgcnyurVr34d0Df{__3_R}43na501" #Shuffled Flag (hard coded in the app)
 
 for i in dex:
     solution += z[int(i)]
